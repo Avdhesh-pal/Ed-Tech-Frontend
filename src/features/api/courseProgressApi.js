a5e7ab51a73f25004cf5d150e631eef5240e570f
Avdhesh Pal <pal709412@gmail.com>
2026-03-28 21:18:49 +0530

    Refactor courseProgressApi to use external API constant

--- src/features/api/courseProgressApi.js
+++ src/features/api/courseProgressApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PROGRESS_API = "http://localhost:8080/api/v1/progress";
+import { COURSE_PROGRESS_API } from "@/lib/api";
 
 export const courseProgressApi = createApi({
   reducerPath: "courseProgressApi",
