1acce0bb03a741f9396d9194208869fd98c3ed93
Avdhesh Pal <pal709412@gmail.com>
2026-03-28 21:19:29 +0530

    Refactor purchaseApi to use centralized API URL
    Refactor purchaseApi to import COURSE_PURCHASE_API from a centralized location.

--- src/features/api/purchaseApi.js
+++ src/features/api/purchaseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PURCHASE_API = "http://localhost:8080/api/v1/purchase";
+import { COURSE_PURCHASE_API } from "@/lib/api";
 
 export const purchaseApi = createApi({
   reducerPath: "purchaseApi",
