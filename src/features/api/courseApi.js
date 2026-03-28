4005267d8cd434f381000b748313adfcf4357e99
Avdhesh Pal <pal709412@gmail.com>
2026-03-28 21:18:00 +0530

    Refactor courseApi to use COURSE_API constant

--- src/features/api/courseApi.js
+++ src/features/api/courseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_API = "http://localhost:8080/api/v1/course";
+import { COURSE_API } from "@/lib/api";
 
 export const courseApi = createApi({
   reducerPath: "courseApi",
